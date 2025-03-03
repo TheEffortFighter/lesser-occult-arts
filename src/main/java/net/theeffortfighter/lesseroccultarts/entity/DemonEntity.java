7fe2f98c0f56e5ce2af16f1181f5699b62d40f43
TheEffortFighter <EffortMixes@outlook.com>
2025-03-02 21:31:52 -0500

    working on demon entity

--- src/main/java/net/theeffortfighter/lesseroccultarts/entity/DemonEntity.java
+++ src/main/java/net/theeffortfighter/lesseroccultarts/entity/DemonEntity.java
@@ -31,55 +31,12 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class DemonEntity extends HostileEntity implements IAnimatable {
+public class DemonEntity extends VexEntity implements IAnimatable {
     private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
     private boolean isAttacking = false; // Track attack animation state
-    public static final float field_30502 = 45.836624f;
-    public static final int field_28645 = MathHelper.ceil(3.9269907f);
-    protected static final TrackedData<Byte> VEX_FLAGS = DataTracker.registerData(VexEntity.class, TrackedDataHandlerRegistry.BYTE);
-    private static final int CHARGING_FLAG = 1;
-    @Nullable
-    MobEntity owner;
-    @Nullable
-    private BlockPos bounds;
-    private boolean alive;
-    private int lifeTicks;
 
-    public DemonEntity(EntityType<? extends HostileEntity> entityType, World world) {
+    public DemonEntity(EntityType<? extends VexEntity> entityType, World world) {
         super(entityType, world);
-    }
-
-    @Override
-    public void move(MovementType movementType, Vec3d movement) {
-        super.move(movementType, movement);
-        this.checkBlockCollision();
-    }
-
-    @Override
-    protected void initGoals() {
-        this.goalSelector.add(8, new LookAtEntityGoal(this, PlayerEntity.class, 8.0f));
-        this.goalSelector.add(8, new LookAroundGoal(this));
-        this.initCustomGoals();
-    }
-
-    protected void initCustomGoals() {
-        this.goalSelector.add(2, new AttackGoal(this));
-        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0));
-        this.targetSelector.add(2, new DemonEntity.TrackOwnerTargetGoal(this));
-        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, IronGolemEntity.class, true));
-    }
-
-    @Override
-    public void tick() {
-        this.noClip = true;
-        super.tick();
-        this.noClip = false;
-        this.setNoGravity(true);
-        if (this.alive && --this.lifeTicks <= 0) {
-            this.lifeTicks = 20;
-            this.damage(DamageSource.STARVE, 1.0f);
-        }
     }
 
     private <E extends IAnimatable> PlayState movementPredicate(AnimationEvent<E> event) {
@@ -113,26 +70,5 @@ public class DemonEntity extends HostileEntity implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return factory;
-    }
-
-    class TrackOwnerTargetGoal
-            extends TrackTargetGoal {
-        private final TargetPredicate targetPredicate;
-
-        public TrackOwnerTargetGoal(PathAwareEntity mob) {
-            super(mob, false);
-            this.targetPredicate = TargetPredicate.createNonAttackable().ignoreVisibility().ignoreDistanceScalingFactor();
-        }
-
-        @Override
-        public boolean canStart() {
-            return DemonEntity.this.owner != null && DemonEntity.this.owner.getTarget() != null && this.canTrack(DemonEntity.this.owner.getTarget(), this.targetPredicate);
-        }
-
-        @Override
-        public void start() {
-            DemonEntity.this.setTarget(DemonEntity.this.owner.getTarget());
-            super.start();
-        }
     }
 }
